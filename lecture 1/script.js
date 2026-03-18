3bf3dcc3755067d78ebb75b14f8c627c4f02aed9
nidzasavic88 <76796519+nidzasavic88@users.noreply.github.com>
2026-03-18 15:30:54 +0100

    Convert lecture 1 to normal folder

---
+++ lecture 1/script.js
@@ -0,0 +1,41 @@
+//Napravili smo podatak koji se zove name, a njegova vrednost je "Nikola Savic"
+let name = "Nikola Savic"; // string = text "" ''
+
+let age = 37; // number -> intiger
+
+let height = 185.5; // number - float - decimal
+
+let isProgramer = true; // bool -> boolean (true/false) true= 1 false = 0
+
+console.log(name,age,height,isProgramer);
+// Vezba 1
+let FristName = "Nikola";
+
+let LastName = "Savic";
+
+let FristNameLastName = FristName+" "+LastName;
+
+console.log(FristNameLastName)
+//Vezba 2
+let FristNumber = 10;
+
+let SecundaryNumber= 15;
+
+let sum = FristNumber+ SecundaryNumber;
+
+console.log(sum);
+
+let cena = 20;
+let kursEvra = 117.2;
+
+let cenaJogurta = 50;
+
+let dinari = cena*kursEvra;
+let jogurtDinari = cenaJogurta*kursEvra;
+console.log(dinari, jogurtDinari);
+
+// Arrey - lista
+// Napravi arrey sa 3 stavke: racunar, tastatura, mis
+
+let components = ["racunar", "tastatura","mis"];
+console.log(components);
---
+++ lecture 1/vezba.js
@@ -0,0 +1,85 @@
+let ime = "Ana";
+console.log(ime);
+
+let godine = 20;
+console.log(godine);
+
+let jeStudent = true;
+console.log(jeStudent);
+
+let grad = "Beograd";
+console.log("Zivim u gradu Beogradu");
+
+let string = "zovem se";
+let predstavljanje = (string+" "+ime);
+console.log(predstavljanje);
+
+let a = 11;
+let b = 4;
+let zbir = (a+b);
+console.log(zbir);
+
+let razlika = (a-b);
+console.log(razlika);
+
+let proizvod = (a*b);
+console.log(proizvod);
+
+let kolicnik = (a/b);
+console.log(kolicnik);
+
+let ostatak = (a%b);
+console.log(ostatak);
+
+let x = 3;
+let y = 4;
+console.log(x + y) * 2;
+
+let ucim = "Ucim programiranje"
+console.log(ucim);
+let zaUcim = ucim.replace("programiranje","kodiranje");
+console.log(zaUcim);
+
+let Danas = "Danas je lep dan";
+console.log(Danas);
+let zaDanas = Danas.replace("lep","suncan");
+console.log(zaDanas);
+
+let Moje = "Moje ime je Ana";
+console.log(Moje);
+let ZaMoje = Moje.replace("Ana","Marko");
+console.log(ZaMoje);
+
+let poruka = "Ucim osnove JavaScript";
+console.log(poruka);
+let ZaPoruke = poruka.replace("osnove","napredne");
+console.log(ZaPoruke);
+
+let broj = 10;
+console.log(broj);
+
+let imam = "Imam";
+let moje = "godine";
+let koja = "godina";
+let sve = (imam+" "+moje+" "+koja);
+console.log(sve);
+
+let jeOnline = false;
+console.log(jeOnline);
+
+let c = 7;
+let d = 2;
+let m = (c+d);
+console.log(m);
+
+let Java = "JavaScript je zabavan";
+console.log(Java);
+let zaJava = Java.replace("zabavan","koristan");
+console.log(zaJava);
+
+let rezultat = (c*d);
+console.log(rezultat);
+
+let rezJe = "Rezultat je";
+let resenje =(rezJe+" "+rezultat);
+console.log(resenje);
