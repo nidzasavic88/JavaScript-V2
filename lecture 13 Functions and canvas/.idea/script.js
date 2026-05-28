a5d8264ecbc19d3ca3b8799d1f741d78db5b1ba7
nidzasavic88 <76796519+nidzasavic88@users.noreply.github.com>
2026-05-27 21:55:43 -0700

    Added lecture 13 canvas

--- lecture 13 Functions and canvas/.idea/script.js
+++ lecture 13 Functions and canvas/.idea/script.js
@@ -8,17 +8,43 @@
 let clickPoints = []; // [x,y] , [x,y]
 
 let canvasElement = document.getElementById("drawArea");
+let ctx = canvasElement.getContext("2d");
 
-canvasElement.addEventListener("click", function() {
+let drawButton = document.getElementById("draw");
+let resetButton = document.getElementById("resetDrawing");
 
-    clickPoints.push("A");
+canvasElement.addEventListener("click", function(event)
+{
+
+    let rect = canvasElement.getBoundingClientRect();
+
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+
+    clickPoints.push({x: x, y: y});
     console.log(clickPoints);
 
-    if (clickPoints < 2)
-    {
-        clickPoints = [];
+
+});
+
+drawButton.addEventListener("click", function()
+{
+    ctx.beginPath();
+    ctx.moveTo(clickPoints[0].x, clickPoints[0].y);
+    for(let i in clickPoints) {
+        if(i == 0) {
+            continue;
+        }
+        ctx.lineTo(clickPoints[i].x, clickPoints[i].y);
     }
 
+    ctx.stroke();
 
+    clickPoints = [];
+
+});
+
+resetButton.addEventListener("click", function()
+{
+    ctx.clearRect(0,0, canvasElement.width, canvasElement.height );
 })
-
